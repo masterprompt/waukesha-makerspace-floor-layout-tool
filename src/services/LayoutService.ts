ffee6163eef6208765ce42be7e4472ba4c6933db
Casey Kerr <masterprompt@gmail.com>
2024-11-06 22:44:54 -0600

    default layout

--- src/services/LayoutService.ts
+++ src/services/LayoutService.ts
@@ -3,6 +3,8 @@ import { pick, assign, find } from "lodash";
 
 export type Layout = string;
 
+const defaultLayout = 'W3siaWQiOiJtYWluQmVuY2giLCJ4Ijo3NTMuNTM3Nzk3OTc3OTI3OCwieSI6MjU1LjAwODIxODIxODE0MDMsInJvdGF0aW9uIjowfSx7ImlkIjoiaGVhdnlNZXRhbFNoZWxmMSIsIngiOi02MTEuMTAwNzc1MTg5NDE3NywieSI6LTYzNC4wNzQ0ODg1NDI0MjIsInJvdGF0aW9uIjowfSx7ImlkIjoiaGVhdnlNZXRhbFNoZWxmMiIsIngiOi00OTMuOTM0ODM3MDg5MTkwNSwieSI6LTYzNC4wNzQ0ODg1NDI0MDI4LCJyb3RhdGlvbiI6MH0seyJpZCI6ImxpcXVpZHNDYWJpbmV0IiwieCI6LTg4MS45MTEyODk4NDgyMzU1LCJ5IjotNjExLjIwMDQ0Mjk2NTQwNSwicm90YXRpb24iOi05MC4wMDAwMDAwMDAwMDAwNn0seyJpZCI6InJlZFRvb2xCb3giLCJ4IjotODkzLjY3NzQ0OTQzMTE0MzUsInkiOi0xNTguNTE4NjIyMTM1NjMxMzUsInJvdGF0aW9uIjotOTB9LHsiaWQiOiJibGFja1Rvb2xCb3giLCJ4IjotODg2Ljc4NTMzNTQyNTI0OTEsInkiOi0yODkuNDY4Nzg4MjQ3NjMwMywicm90YXRpb24iOi04OS45OTk5OTk5OTk5OTk4N31d';
+
 export class LayoutService {
     private layoutUrlKey = 'layout';
     generateFurnitureLayoutBase64 (furniture: IImage[]) {
@@ -26,7 +28,7 @@ export class LayoutService {
 
     getLayoutFromUrl () {
         const url = new URL(window.location.href);
-        return url.searchParams.get(this.layoutUrlKey);
+        return url.searchParams.get(this.layoutUrlKey) || defaultLayout;
     }
 
     getLayoutUrl (layout: Layout) {
