52ed779f89059fda20c020f442695446d99bc1dd
Casey Kerr <masterprompt@gmail.com>
2024-11-06 22:38:00 -0600

    assets

--- src/furniture/index.ts
+++ src/furniture/index.ts
@@ -1,7 +1,11 @@
 import { IImage } from "../types";
 
-import floorPlanImage from '../assets/floor-plan.jpg';
-import mainBench from '../assets/Main-Bench.png';
+import floorPlanImage from './floor-plan.jpg';
+import mainBench from './Main-Bench.png';
+import heavyMetalShelf from './heavyMetalShelf.png';
+import liquidsCabinet from './liquids-cabinet.png';
+import blackToolBox from './black-tool-box.png';
+import redToolBox from './red-tool-box.png';
 
 const images: IImage[] = [
     {
@@ -18,7 +22,47 @@ const images: IImage[] = [
         y: 0,
         src: mainBench,
         draggable: true,
-        rotation: 90
+        rotation: 0
+      },
+      {
+        id: 'heavyMetalShelf1',
+        x: 0,
+        y: 0,
+        src: heavyMetalShelf,
+        draggable: true,
+        rotation: 0
+      },
+      {
+        id: 'heavyMetalShelf2',
+        x: 0,
+        y: 0,
+        src: heavyMetalShelf,
+        draggable: true,
+        rotation: 0
+      },
+      {
+        id: 'liquidsCabinet',
+        x: 0,
+        y: 0,
+        src: liquidsCabinet,
+        draggable: true,
+        rotation: 0
+      },
+      {
+        id: 'redToolBox',
+        x: 0,
+        y: 0,
+        src: redToolBox,
+        draggable: true,
+        rotation: 0
+      },
+      {
+        id: 'blackToolBox',
+        x: 0,
+        y: 0,
+        src: blackToolBox,
+        draggable: true,
+        rotation: 0
       },
 ];
 
